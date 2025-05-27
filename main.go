f0fbabbc1aea55abda0653adda455debd95cfc81
Steven Wilkin <stevenwilkin@gmail.com>
2025-05-27 19:29:56 +0700

    Use new feed handler

--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 	"os"
 	"time"
 
+	"github.com/stevenwilkin/carry/feed"
 	"github.com/stevenwilkin/swaps/binance"
 	"github.com/stevenwilkin/swaps/bybit"
 	"github.com/stevenwilkin/swaps/deribit"
 )
 
 var (
+	h  = feed.NewHandler()
 	b  = &binance.Binance{}
 	by = &bybit.Bybit{}
 	d  = &deribit.Deribit{}
@@ -36,32 +38,40 @@ func display() {
 	fmt.Printf("  Deribit: %9.2f %s\n", sDeribit, delta(spot, sDeribit))
 }
 
-func process[T any](f func() chan T, p func(x T)) {
-	go func() {
-		for result := range f() {
-			p(result)
-		}
-		os.Exit(1)
-	}()
+func displayFailing() {
+	fmt.Println("\033[2J\033[H\033[?25l") // clear screen, move cursor to top of screen, hide cursor
+	fmt.Println("Feed failing...")
+}
+
+func exitFailed() {
+	fmt.Println("Feed failed")
+	os.Exit(1)
 }
 
 func main() {
-	process(b.Price, func(x float64) {
+	h.Add(feed.NewFeed(b.Price, func(x float64) {
 		spot = x
-	})
+	}))
 
-	process(by.Price, func(x float64) {
+	h.Add(feed.NewFeed(by.Price, func(x float64) {
 		sBybit = x
-	})
+	}))
 
-	process(d.Price, func(x float64) {
+	h.Add(feed.NewFeed(d.Price, func(x float64) {
 		sDeribit = x
-	})
+	}))
 
 	t := time.NewTicker(100 * time.Millisecond)
 
 	for {
-		display()
+		if h.Failed() {
+			exitFailed()
+		} else if h.Failing() {
+			displayFailing()
+		} else {
+			display()
+		}
+
 		<-t.C
 	}
 }
